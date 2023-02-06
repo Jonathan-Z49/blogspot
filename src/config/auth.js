47fb49c38c71eb6a76f9b91d49559c7e436bcd99
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-02-06 01:25:43 -0600

    Updated passport google strategy

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,15 +5,42 @@ const User = require("../models/userModel");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://yourdomain:3000/auth/google/callback",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback",
       passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return done(err, user);
-      });
+      try {
+        const { id, given_name, family_name } = profile;
+        const photo_url = profile.photos[0].value;
+        const existing_user = await User.findOne({ googleId: id });
+
+        if (!existing_user) {
+          const new_user = new User({
+            googleId: id,
+            first_name: given_name,
+            last_name: family_name,
+            photo: photo_url,
+          });
+
+          await new_user.save();
+          return done(null, new_user);
+        }
+
+        return done(null, existing_user);
+      } catch (error) {
+        console.error(error);
+      }
     }
   )
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const currentUser = await User.findOne({ id });
+  done(null, currentUser);
+});
