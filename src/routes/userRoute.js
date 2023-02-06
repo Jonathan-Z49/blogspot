fcdf2885f3d984a16ed2394a3f8a828de326ec5e
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-02-06 01:28:28 -0600

    removed user route

--- src/routes/userRoute.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-router.get("/:id", userController.user_by_id);
