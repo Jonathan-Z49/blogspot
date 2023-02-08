3b299853055023157c369601b8f1b863e89c2516
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-02-07 18:44:31 -0600

    Create user context for user state management

---
+++ src/contexts/userContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+
+export const initUser: User = {
+  first_name: '',
+  last_name: '',
+  photo: '',
+  posts: [],
+  comments: [],
+  logged_in: false,
+};
+
+export const userContext = createContext<{
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+}>({ user: initUser, setUser: () => {} });
