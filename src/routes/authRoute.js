24ee4b5e12b0e4b7b9d2330b45c276be76ff76ee
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-02-12 03:56:30 -0600

    Added auth routes to handle all authentication

---
+++ src/routes/authRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const passport = require("passport");
+require("../config/auth");
+
+const router = express.Router();
+
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.json(req.user);
+  } else {
+    res.json({ status: "Login failed" });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+});
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"],
+  })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: process.env.CLIENT_ORIGIN_FAILURE,
+    successRedirect: process.env.CLIENT_ORIGIN,
+  })
+);
+
+module.exports = router;
