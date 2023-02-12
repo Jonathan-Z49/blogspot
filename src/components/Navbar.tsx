1f1497142b1a043e47df56e2fef2fa9ec8a6f8f9
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-02-12 03:51:47 -0600

    Added links to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,14 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-link">
-          <Link to="/profile">Profile</Link>
+        <li className="navbar-link navbar-logo">
+          <Link to="/">Blogspot</Link>
         </li>
         <li className="navbar-link">
           <Link to="/posts">Blogs</Link>
+        </li>
+        <li className="navbar-link">
+          <Link to="/profile">Profile</Link>
         </li>
         <li className="navbar-link">
           <Link to="/login">Sign in</Link>
