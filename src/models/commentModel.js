0a09c048cc3932eb6f07c39817e655b64482b42b
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-03-31 13:49:32 -0500

    Updated comment model to include reference to a post document for ease of deletion

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  post: { type: Schema.Types.ObjectId, required: true, ref: "Post" },
   body: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
