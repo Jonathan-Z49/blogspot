d9f06c9f499e896bb543f61e12b446686911515a
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-03-31 13:45:03 -0500

    Updated comment controller update/delete method for document references in other db models

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,5 +1,6 @@
 const Comment = require("../models/commentModel");
 const Post = require("../models/postModel");
+const User = require("../models/userModel");
 const mongoose = require("mongoose");
 
 exports.comment_list_by_blog = async (req, res) => {
@@ -16,9 +17,21 @@ exports.comment_list_by_blog = async (req, res) => {
 
 exports.comment_create = async (req, res) => {
   try {
-    const comment = new Comment({ author: req.user.id, body: req.body.body });
     const postId = mongoose.Types.ObjectId(req.params.id);
-    await Post.findAndUpdate({ _id: postId }, { $push: { comments: comment } });
+    const comment = new Comment({
+      author: req.user.id,
+      body: req.body.body,
+      post: mongoose.Types.ObjectId(postId),
+    });
+    await comment.save();
+    await Post.findOneAndUpdate(
+      { _id: postId },
+      { $push: { comments: comment } }
+    );
+    await User.findOneAndUpdate(
+      { _id: req.user.id },
+      { $push: { comments: comment } }
+    );
     res.json({ status: "Comment created" });
   } catch (error) {
     console.error(error);
@@ -37,9 +50,18 @@ exports.comment_update = async (req, res) => {
 };
 exports.comment_delete = async (req, res) => {
   try {
-    await Comment.findByIdAndDelete({
-      _id: mongoose.Types.ObjectId(req.params.id),
-    });
+    const cmt = await Comment.findOneAndDelete(
+      { _id: req.params.id },
+      { body: req.body.body }
+    );
+    await User.findOneAndUpdate(
+      { _id: cmt.author },
+      { $pull: { comments: req.params.id } }
+    );
+    await Post.findOneAndUpdate(
+      { _id: cmt.post },
+      { $pull: { comments: req.params.id } }
+    );
     res.json({ status: "Comment deleted" });
   } catch (error) {
     console.error(error);
