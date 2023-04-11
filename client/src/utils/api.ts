695c6984a128abd981043e9a4eba6fcc489c94e2
Jonathan Zarate <jonathanz49.dev@gmail.com>
2023-04-11 17:28:55 -0500

    changed server url in api

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,10 +1,13 @@
 export async function getAllPosts() {
   try {
-    const response = await fetch('http://localhost:5000/posts', {
-      method: 'GET',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      'https://blog-server-production-82ec.up.railway.app:5000/posts',
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response: Post[] = await response.json();
     return json_response;
   } catch (error) {
@@ -14,11 +17,14 @@ export async function getAllPosts() {
 
 export async function getPostById(id: string) {
   try {
-    const response = await fetch(`http://localhost:5000/posts/${id}`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/posts/${id}`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response: Post = await response.json();
     return json_response;
   } catch (error) {
@@ -28,11 +34,14 @@ export async function getPostById(id: string) {
 
 export async function getPostsByUser(id: string) {
   try {
-    const response = await fetch(`http://localhost:5000/posts/user/${id}`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/posts/user/${id}`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response: Post[] = await response.json();
     return json_response;
   } catch (error) {
@@ -42,12 +51,15 @@ export async function getPostsByUser(id: string) {
 
 export async function createPost(post: { title: string; body: string }) {
   try {
-    const response = await fetch('http://localhost:5000/posts', {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(post),
-    });
+    const response = await fetch(
+      'https://blog-server-production-82ec.up.railway.app:5000/posts',
+      {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(post),
+      },
+    );
     const json_response = await response.json();
     return json_response;
   } catch (error) {
@@ -57,12 +69,15 @@ export async function createPost(post: { title: string; body: string }) {
 
 export async function updatePost(post: { title?: string; body?: string }, id: string) {
   try {
-    const response = await fetch(`http://localhost:5000/posts/${id}`, {
-      method: 'PUT',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(post),
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/posts/${id}`,
+      {
+        method: 'PUT',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(post),
+      },
+    );
     const json_response = await response.json();
     return json_response;
   } catch (error) {
@@ -73,11 +88,14 @@ export async function updatePost(post: { title?: string; body?: string }, id: st
 export async function deletePost(id: string) {
   try {
     if (id) {
-      const response = await fetch(`http://localhost:5000/posts/${id}`, {
-        method: 'DELETE',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-      });
+      const response = await fetch(
+        `https://blog-server-production-82ec.up.railway.app:5000/posts/${id}`,
+        {
+          method: 'DELETE',
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
       const json_response = await response.json();
       return json_response;
     }
@@ -89,11 +107,14 @@ export async function deletePost(id: string) {
 
 export async function commentsByPost(postId: string) {
   try {
-    const response = await fetch(`http://localhost:5000/comments/posts/${postId}`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/comments/posts/${postId}`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response: Comment[] = await response.json();
     return json_response;
   } catch (error) {
@@ -103,12 +124,15 @@ export async function commentsByPost(postId: string) {
 
 export async function createComment(obj: { body: string }, postId: string) {
   try {
-    const response = await fetch(`http://localhost:5000/comments/posts/${postId}`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(obj),
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/comments/posts/${postId}`,
+      {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(obj),
+      },
+    );
     const json_response = await response.json();
     return json_response;
   } catch (error) {
@@ -118,12 +142,15 @@ export async function createComment(obj: { body: string }, postId: string) {
 
 export async function updateComment(obj: { body: string }, id: string) {
   try {
-    const response = await fetch(`http://localhost:5000/comments/${id}`, {
-      method: 'PUT',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(obj),
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/comments/${id}`,
+      {
+        method: 'PUT',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(obj),
+      },
+    );
     const json_response = await response.json();
     return json_response;
   } catch (error) {
@@ -133,11 +160,14 @@ export async function updateComment(obj: { body: string }, id: string) {
 
 export async function deleteComment(id: string) {
   try {
-    const response = await fetch(`http://localhost:5000/comments/${id}`, {
-      method: 'DELETE',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/comments/${id}`,
+      {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response = await response.json();
     return json_response;
   } catch (error) {
@@ -147,11 +177,14 @@ export async function deleteComment(id: string) {
 
 export async function getUserData() {
   try {
-    const response = await fetch(`http://localhost:5000/login/success`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/login/success`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response: User | { status: string } = await response.json();
     return json_response;
   } catch (error) {
@@ -161,11 +194,14 @@ export async function getUserData() {
 
 export async function userLogout() {
   try {
-    const response = await fetch(`http://localhost:5000/logout`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `https://blog-server-production-82ec.up.railway.app:5000/logout`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const json_response: { status: string } = await response.json();
     console.log(json_response);
 
